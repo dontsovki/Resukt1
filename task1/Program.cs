16bb4ab2dd4a52e92d8b365450e75f08bf1fd5bc
Artem.don <dontsovki@gmail.com>
2022-12-18 10:39:27 +0300

    Из отрезка (1-100) находим наибольшие число

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Из отрезка (1-100) находим наибольшие число
+
+
+int number = new Random().Next(10,100);
+Console.WriteLine(number);
+
+if (number/10 > number%10)  //сравнение
+{
+    Console.WriteLine(number/10);
+}
+else
+{
+    Console.WriteLine(number%10);
+}
